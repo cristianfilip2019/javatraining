e58d43ffddd6ffa3e7d11baa4129c299cdc4e106
servi <servi@DESKTOP-3213N73>
2020-03-16 15:04:25 +0200

    First commit to cristianfilip.2019@gmail.com repository - JavaApplication2

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,19 @@
+/*
+ * test repository new created
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author servi
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("this is a new message!");
+    }
+    
+}
